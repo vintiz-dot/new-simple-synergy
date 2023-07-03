4e0e40e51113434df3b3d7d89a5acd0787ae7fc4
vintiz-dot <vintizfjes@gmail.com>
2023-07-03 05:25:27 -0400

    update

---
+++ pricing-script.js
@@ -0,0 +1,14 @@
+"use strict";
+const priceContainer = document.querySelector(".pricing");
+const priceBox = document.querySelectorAll(".box");
+
+// activating the box thats clicked
+// priceContainer.addEventListener("click", function (x) {
+//   x.preventDefault();
+//   if (x.target.classList.contains("box") || x.target.closest(".box")) {
+//     priceBox.forEach(function (x) {
+//       x.classList.remove("featured");
+//     });
+//     x.target.closest(".box").classList.add("featured");
+//   }
+// });
